ed264df66c1998a29e740196afd69b7edda242b1
caboosecodes <ricardolopez413@hotmail.com>
2021-07-21 15:30:34 -0700

    create AcmeAccountingSystems solution

---
+++ AcmeAccountingSystems - Writing and Running/Program.cs
@@ -0,0 +1,16 @@
+﻿// This is an assignment for The Tech Academy to print to the console
+
+using System;
+
+
+namespace AcmeAccountingSystems___Writing_and_Running
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
