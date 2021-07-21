7d9d1fa3b0ec370418d86a6adede4fee5e64f800
caboosecodes <ricardolopez413@hotmail.com>
2021-07-21 16:35:35 -0700

    Writing code while following along with video instructions

--- VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Following along with video instrcution from The Tech Academy
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,25 @@ namespace VariablesAndDataTypes
     {
         static void Main(string[] args)
         {
+            //// var says that something is a variable; NOT THE TYPE
+            //// C# is a statically typed language; state the datatype before declaring a variable
+            //// This is a simple program
+            //console.writeline("what is your name?");
+            //// ReadLine() takes in the value
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your Name is: " + yourName);
+            //Console.ReadLine();
+            //Console.WriteLine("what is your favorite number?");
+            //// Casting is the practice of action of changing one piece of data to another data type
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your Favorite Number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursworked = 42
+            
         }
     }
 }
