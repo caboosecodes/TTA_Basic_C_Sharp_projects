e5684277721ed3f584dd44862e221298ca158523
caboosecodes <ricardolopez413@hotmail.com>
2021-07-23 11:52:57 -0700

    Create and write code for boolean logic submission assignment.

---
+++ BooleanInsurance/Program.cs
@@ -0,0 +1,35 @@
+﻿// Boolean Logic Submission Assignment from The Tech Academy
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanInsurance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Car Insurance Application");
+            // asks user's Age
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            // asks if the user has a DUI
+            Console.WriteLine("Have you ever had a DUI, please enter \"true\" or \"false\"");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+
+            // asks the user how many tickets they have
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            // boolean logic using the user's answers to determine if they are qualified for car insurance
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(age > 15 && dui == false && tickets <=3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BooleanLogic/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // and operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            // Or operator
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(false || false);
+
+            // equal to operator
+            //Console.WriteLine(true == true);
+            //Console.WriteLine(true == false);
+            //Console.WriteLine(false == false);
+
+            // not equal to
+            //Console.WriteLine(true != true);
+            //Console.WriteLine(true != false);
+            //Console.WriteLine(false != false);
+
+            // xor operator will evalutates as true if only one is true but not both
+            //Console.WriteLine(true ^ true);
+            //Console.WriteLine(true ^ false);
+            //Console.WriteLine(false ^ false);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
