2a5a34e67f839b4a8a285593bf9c9944b86a913a
caboosecodes <ricardolopez413@hotmail.com>
2021-07-27 14:14:42 -0700

    Create ArrayIterationAssignment and write code for part one of the assignment

---
+++ ArrayIterationAssignemnt/Program.cs
@@ -0,0 +1,38 @@
+﻿// Six part assignment
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayIterationAssignemnt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part One: Append user input to string element in array
+            // initializes an array "questions"
+            string[] randomColors = { "Blue ", "Black ", "White ", "Red ", "Green ", "Yellow ", "Purple " };
+
+            // Asks for user input and saves it to a variable
+            Console.WriteLine("Please enter in a color");
+            string userColor = Console.ReadLine();
+            // for loop to iterate through the array
+
+            // for loop
+            for (int i= 0; i < randomColors.Length; i++)
+            {
+                // appends the userColor to the string at the index i in the array randomColors
+                randomColors[i] = randomColors[i] + userColor;
+                // Prints the string at the index i in the array randomColors
+                Console.WriteLine(randomColors[i]);
+            }
+
+            // Part Two
+
+            Console.ReadLine();
+        }
+    }
+}
