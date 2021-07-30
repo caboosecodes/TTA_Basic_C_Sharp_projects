00dfcf1360d7c0310af1aef33ff9a4063bc93558
caboosecodes <ricardolopez413@hotmail.com>
2021-07-30 09:59:01 -0700

    write try catch block for tech academy assignment

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // exception handling are protections in the code that checks for particular errors and responds to those errors accordingly
+        // what we want to do goes in the try block
+        try 
+        {
+            Console.WriteLine("Pick a number.");
+            int numberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("pick a second number.");
+            int numberTwo = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("dividing the two.... ");
+            int numberThree = numberOne / numberTwo;
+            Console.WriteLine(numberOne + " divided by " + numberTwo + " equals " + numberThree);
+            Console.ReadLine();
+        }
+        // catching the FormatException and assinging it the variable "ex"
+        // ex is a datatype for FormatException
+        catch (FormatException ex)
+        {
+            Console.WriteLine("please type a whole number.");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Please don't divide by zero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        // finally runs no matter what
+        finally
+        {
+            Console.ReadLine();
+        }
+        
+
+        
+    }
+}
---
+++ TryCatchAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // initialize a list of integers named numbers
+            List<int> numbers = new List<int>();
+            // add numbers to that list
+            numbers.Add(6);
+            numbers.Add(10);
+            numbers.Add(15);
+            numbers.Add(25);
+            numbers.Add(45);
+            numbers.Add(82);
+            numbers.Add(94);
+
+            try
+            {
+                // ask the user for a number
+                Console.Write("Please select a non-zero number to divide by: ");
+                int userNumber = Convert.ToInt32(Console.ReadLine());
+
+                // iterates through numbers list
+                foreach (int number in numbers)
+                {
+                    // divides the number in the list by the user's input
+                    int division = number / userNumber;
+                    // prints when the user inputs a whole number
+                    Console.WriteLine(number + " divided by " + userNumber + " is equal to: " + division);
+                }
+            }
+            // divide by zero error
+            catch (DivideByZeroException ex)
+            {
+                // prints when the user tries to divide by zero
+                Console.WriteLine("Don't divide by zero.");
+            }
+            // divide by non-number
+            catch (FormatException ex)
+            {
+                // prints when the user inputs a non-number
+                Console.WriteLine("Please type a whole number.");
+            }
+            // general exception
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // finally block runs no matter what
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
