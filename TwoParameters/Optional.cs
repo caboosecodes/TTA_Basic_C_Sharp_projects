0a16cea9ddd33b511e4ea6d1d8702cddb78d8765
caboosecodes <ricardolopez413@hotmail.com>
2021-08-02 12:21:49 -0700

    create and write code for Method Submission Assignment

---
+++ TwoParameters/Optional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class Optional
+    {
+        public static int Two(int num1, int num2 = 1)
+        {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
---
+++ TwoParameters/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare variables
+            int num1, num2;
+            int result;
+
+            // asks user for a number
+            Console.WriteLine("Pick a number: ");
+            // if the user input is not an accepted value of try parse
+            // out lets the data value be passed to num1
+            int.TryParse(Console.ReadLine(), out num1);
+            
+            // asks user to second number
+            Console.WriteLine("Pick another number (optional)");
+            // if user inputs a second number
+            if (int.TryParse(Console.ReadLine(), out num2))
+            {
+                // passes bothes parameters into the class method
+                result = Optional.Two(num1, num2);
+                Console.WriteLine("{0} minus {1} equals {2}", num1, num2, result);
+            }
+            // if ther user doesn't input a number
+            else
+            {
+                // it only passes the first number as the parameter
+                result = Optional.Two(num1);
+                Console.WriteLine("{0} minus 1 equals {1}", num1, result);
+            }
+
+            Console.ReadLine();
+
+            //Console.WriteLine("Please input two numbers");
+            //Console.Write("Number 1: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Number 2 (optional): ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            
+            //Console.WriteLine("{0} minus {1} equals {2}", num1, num2, Optional.Two(num1, num2));
+            //Console.ReadLine();
+        }
+    }
+}
