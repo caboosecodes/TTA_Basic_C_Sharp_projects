61092fa0a69bc36eb925b7321b81b51b77711398
caboosecodes <ricardolopez413@hotmail.com>
2021-08-10 16:48:34 -0700

    write code for admin view and admincontroller

---
+++ CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,35 @@
+﻿using CarInsurance.Models;
+using CarInsurance.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var insurees = db.Insurees.ToList();
+                var insureeVms = new List<InsureeVm>();
+                
+                foreach (var insuree in insurees)
+                {
+                    var insureeVm = new InsureeVm();
+                    insureeVm.Quote = insuree.Quote;
+                    insureeVm.FirstName = insuree.FirstName;
+                    insureeVm.LastName = insuree.LastName;
+                    insureeVm.EmailAddress = insuree.EmailAddress;
+                    insureeVms.Add(insureeVm);
+                }
+                return View(insureeVms);
+            }
+            
+        }
+    }
+}
