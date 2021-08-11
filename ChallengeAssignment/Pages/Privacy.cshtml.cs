2c361c10b6d04e6e23431dfef9aaa310ba3c5f68
caboosecodes <ricardolopez413@hotmail.com>
2021-08-11 08:33:31 -0700

    create and write code for code first databse

---
+++ ChallengeAssignment/Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace ChallengeAssignment.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var dateTime = DateTime.Now.ToLocalTime();
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+}
---
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var db = new StudentContext())
+            {
+                // creating a new student
+                Console.WriteLine("Enter Student's first name");
+                var firstName = Console.ReadLine();
+                Console.WriteLine("Enter the Student's last name");
+                var lastName = Console.ReadLine();
+                Console.WriteLine("Enter the student's test scores (whole numbers please)");
+                var testScores = Convert.ToInt32(Console.ReadLine());
+                //save student to database
+                var student = new Student
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    TestScores = testScores
+                };
+                db.Students.Add(student);
+                db.SaveChanges();
+
+                Console.WriteLine(student.FirstName + " " + student.LastName + " has been saved to the database");
+                            
+            }
+            Console.ReadLine();
+        }
+    }
+    //public class School
+    //{
+    //    public int SchoolId { get; set; }
+    //    public string SchoolName { get; set; }
+
+    //    public virtual List<Student> Students { get; set; }
+    //}
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TestScores { get; set; }
+
+        //public int SchoolId { get; set; }
+        //public virtual School School { get; set; }
+
+    }
+
+    public class StudentContext : DbContext
+    {
+        //public DbSet<School> Schools { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
